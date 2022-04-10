604fdc491413e6918043d1558699a44eb5e6f653
Ryan Henry Njoroge <kaparocorp@gmail.com>
2022-04-10 15:26:43 +0300

    finished adding the mouseover and mouseout functionality
    finished with image viewing display

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -7,9 +7,42 @@ let images=["../pics/lizar.png","../pics/thika.jpeg","../pics/rongo.jpeg"]
 let count=0;
 
 preview.forEach((e)=>{
-    e.addEventListener("click", changePic)
+    e.addEventListener("mouseover", ()=>{
+        if(e == preview[0]){
+            console.log("workes")
+            imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
+        }else if(e == preview[1]){
+            console.log("2 also works")
+            imgScreen.style.backgroundImage=`url(../pics/thika.jpeg)`
+        }else if(e == preview[2]){
+            console.log("3 also works")
+            imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
+        }else{
+            console.log("obviously!")
+            imgScreen.style.backgroundImage=`url(../pics/rongo.jpeg)`
+        }
+    })
+});
+preview.forEach((e)=>{
+    e.addEventListener("mouseout", ()=>{
+        if(e == preview[0]){
+            console.log("workes")
+            imgScreen.style.backgroundImage=``
+        }else if(e == preview[1]){
+            console.log("2 also works")
+            imgScreen.style.backgroundImage=``
+        }else if(e == preview[2]){
+            console.log("3 also works")
+            imgScreen.style.backgroundImage=``
+        }else{
+            console.log("obviously!")
+            imgScreen.style.backgroundImage=``
+        }
+    })
 });
 
-function changePic(){
-    imgScreen.style.backgroundImage=`url(${images[0]})`
-};
+
+
+// function changePic(){
+//     imgScreen.style.backgroundImage=`url(${images[0]})`
+// };
