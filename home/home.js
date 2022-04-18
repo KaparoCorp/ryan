4c4782c21a54463d8fe72cd0302577e774ba24c8
Ryan Henry Njoroge <kaparocorp@gmail.com>
2022-04-18 18:09:59 +0300

    removed  console logs

--- home/home.js
+++ home/home.js
@@ -8,7 +8,6 @@ userInput.addEventListener("keyup",()=>{
 let imgScreen = document.querySelector(".imgDisplay");
 let preview = document.querySelectorAll(".timeline__blub");
 
-console.log(imgScreen)
 
 let images=["../pics/lizar.png","../pics/thika.jpeg","../pics/rongo.jpeg"]
 let count=0;
@@ -16,16 +15,12 @@ let count=0;
 preview.forEach((e)=>{
     e.addEventListener("mouseover", ()=>{
         if(e == preview[0]){
-            console.log("workes")
             imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
         }else if(e == preview[1]){
-            console.log("2 also works")
             imgScreen.style.backgroundImage=`url(../pics/thika.jpeg)`
         }else if(e == preview[2]){
-            console.log("3 also works")
             imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
         }else{
-            console.log("obviously!")
             imgScreen.style.backgroundImage=`url(../pics/rongo.jpeg)`
         }
     })
@@ -33,16 +28,12 @@ preview.forEach((e)=>{
 preview.forEach((e)=>{
     e.addEventListener("mouseout", ()=>{
         if(e == preview[0]){
-            console.log("workes")
             imgScreen.style.backgroundImage=``
         }else if(e == preview[1]){
-            console.log("2 also works")
             imgScreen.style.backgroundImage=``
         }else if(e == preview[2]){
-            console.log("3 also works")
             imgScreen.style.backgroundImage=``
         }else{
-            console.log("obviously!")
             imgScreen.style.backgroundImage=``
         }
     })
