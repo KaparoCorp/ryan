c9cdd0a301077b1e09b83b8da0081585ae574bd3
KaparoCorp <kaparocorp@gmail.com>
2024-05-15 12:27:36 +0300

    changed site links

--- home.js
+++ home.js
@@ -9,19 +9,19 @@ let imgScreen = document.querySelector(".imgDisplay");
 let preview = document.querySelectorAll(".timeline__blub");
 
 
-let images=["../pics/lizar.png","../pics/thika.jpeg","../pics/rongo.jpeg"]
+let images=["pics/lizar.png","pics/thika.jpeg","pics/rongo.jpeg"]
 let count=0;
 
 preview.forEach((e)=>{
     e.addEventListener("mouseover", ()=>{
         if(e == preview[0]){
-            imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
+            imgScreen.style.backgroundImage=`url(pics/lizar.png)`
         }else if(e == preview[1]){
-            imgScreen.style.backgroundImage=`url(../pics/thika.jpeg)`
+            imgScreen.style.backgroundImage=`url(pics/thika.jpeg)`
         }else if(e == preview[2]){
-            imgScreen.style.backgroundImage=`url(../pics/lizar.png)`
+            imgScreen.style.backgroundImage=`url(pics/lizar.png)`
         }else{
-            imgScreen.style.backgroundImage=`url(../pics/rongo.jpeg)`
+            imgScreen.style.backgroundImage=`url(pics/rongo.jpeg)`
         }
     })
 });
